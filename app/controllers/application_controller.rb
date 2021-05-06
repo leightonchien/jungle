bfded10282908fc9a272f7acaf3f3a9c83a94981
leightonchien <leightonchien@gmail.com>
2021-05-06 20:18:25 +0000

    debug car render + fix final total

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require "action_view"
-include ActionView::Helpers::NumberHelper
 
 
 class ApplicationController < ActionController::Base
@@ -20,9 +18,7 @@ class ApplicationController < ActionController::Base
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
-    number_to_currency(
-      enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum / 100.00, 
-      options = { precision: 2, delimiter: "," })
+    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum / 100.00
   end
   helper_method :cart_subtotal_cents
 
