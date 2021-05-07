e76a0778176e0b76b94eac112bab84b71ea78418
leightonchien <leightonchien@gmail.com>
2021-05-07 02:26:17 +0000

    add admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @category = Category.all
+    @product = Product.all
   end
+  
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,23 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
+  #need to create an index, new and create actions
+  def index
+    @category_name_to_id = {}
+    @product_per_category_count = {}
+    @product_category = Category.joins(:products)
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+    @product_category.map do |n|
+      unless @product_per_category_count[n.name] == nil
+        @product_per_category_count[n.name] += 1
+      else
+        @product_per_category_count[n.name] = 1
+        @category_name_to_id[n.name] = n.id
+      end
+    end
   end
 
+  def new
+  end
+
+  def create
+  end
 end
